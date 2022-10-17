b93bb5b577a33201bfccc95b1cbb7b2a3eda3ebf
oscar Duarte duarte sepulveda <oscarjose93@gmail.com>
2022-10-17 13:01:45 -0600

    cambio de url

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -11,7 +11,6 @@ class CitaController {
         session_start();
         isAuth();
 
-
         $router->render('cita/index',[
             'nombre' => $_SESSION['nombre'],
             'id' => $_SESSION['id']
